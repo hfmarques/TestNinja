6589b9d9c86b608f6176dc61f8ebd8b274ade494
Heber Marques <heber.marques@umusic.com>
2022-08-01 08:16:47 -0300

    Removed FakeObject and added Moq

--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -1,3 +1,4 @@
+using Moq;
 using NUnit.Framework;
 using TestNinja.Mocking;
 using TestNinja.UnitTests.Fundamentals;
@@ -7,13 +8,23 @@ namespace TestNinja.UnitTests.Mocking;
 [TestFixture]
 public class VideoServiceTests
 {
+    private Mock<IFileReader> fileReader;
+    private VideoService service;
+
+    [SetUp]
+    public void SetUp()
+    {
+        fileReader = new Mock<IFileReader>();
+        service = new VideoService(fileReader.Object);
+    }
+
     [Test]
     public void ReadVideoTitle_EmptyFile_ReturnError()
     {
-        var service = new VideoService(new FakeFileReader());
+        fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
 
         var result = service.ReadVideoTitle();
-        
+
         Assert.That(result, Does.Contain("error").IgnoreCase);
     }
 }
